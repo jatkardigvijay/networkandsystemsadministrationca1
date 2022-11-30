c68e8ea1773136bdbd7a7ab4a3ad6d112cbc33d6
Digvijay Jatkar <jatkardigvijay@gmail.com>
2022-11-30 22:45:15 +0000

    welcome message

--- src/main/java/com/jbd/NetworkAndSystemsAdministrationApplication.java
+++ src/main/java/com/jbd/NetworkAndSystemsAdministrationApplication.java
@@ -1,26 +1,19 @@
 package com.jbd;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/employee")
 public class NetworkAndSystemsAdministrationApplication {
-	
-	@Autowired
-    private EmployeeDao employeeDao;
 
-    @GetMapping("/api/v1")
-    public List<Employee> fetchOrders() {
-        return employeeDao.getEmployees();
-    }
+	@GetMapping("/home")
+	public String getMessage() {
+
+		return "Welcome AWS Code Pipeline";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetworkAndSystemsAdministrationApplication.class, args);
