56c2945bce441b450e03b0e5242dc331b9393a2b
Digvijay Jatkar <jatkardigvijay@gmail.com>
2022-12-01 21:45:30 +0000

    REST API added

--- src/main/java/com/jbd/EmployeeDao.java
+++ src/main/java/com/jbd/EmployeeDao.java
@@ -13,8 +13,10 @@ public class EmployeeDao {
     public List<Employee> getEmployees() {
         return Stream.of(
                 new Employee(1, "Digvijay", "Jatkar", "ranadigvijay@gmail.com"),
-                new Employee(1, "Devi", "Prasad", "prsaddevi@gmail.com"),
-                new Employee(1, "Ramesh", "Lala", "lalaramesh@gmail.com"))
+                new Employee(2, "Devi", "Prasad", "prsaddevi@gmail.com"),
+                new Employee(3, "Ankan", "Joshi", "joshiankan@gmail.com"),
+                new Employee(4, "Devi", "yadav", "prsadyadav@gmail.com"),
+                new Employee(5, "Ramesh", "Lala", "lalaramesh@gmail.com"))
 
                 .collect(Collectors.toList());
     }
